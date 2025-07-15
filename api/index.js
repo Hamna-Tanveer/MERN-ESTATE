d514d8f6b66baab23f47582b641e4e3413813ce5
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-07-15 08:22:59 +0500

    create mongodb and connect

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"));
 const app = express();
 app.listen(3000, () => console.log("Server running at port 3000"));
