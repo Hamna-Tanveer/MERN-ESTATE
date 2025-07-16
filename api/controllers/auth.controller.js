bf23af2fc3c9cb7506dddaec578724a2a3454b1e
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-07-16 20:12:44 +0500

    create signup api routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hasedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hasedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json("User created succesfully!");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,11 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"));
 
+// middleware
+app.use(express.json());
+
+// set routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => console.log("Server running at port 3000"));
