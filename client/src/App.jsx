344f6a682c58ad6cc8013b315e58ce4f0a9c2121
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-07-16 20:50:53 +0500

    complete sign up page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/singin" element={<Signin />}></Route>
-        <Route path="/signout" element={<Signup />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
