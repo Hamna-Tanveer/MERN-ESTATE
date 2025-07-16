dfcd0e150b508f7ee55146bcae9d9d50577a9b84
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-07-16 20:31:45 +0500

    modify error handler function

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,5 +1,5 @@
 export const errorHandler = (statusCode, message) => {
-  const error = new Error(message); // JS error constructor
+  const error = new Error(); // JS error constructor
   error.statusCode = statusCode;
   error.message = message;
   return error;
