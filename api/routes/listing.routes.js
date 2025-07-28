4dc888c2fcec9ad44937b0d41d1334a78914347c
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-07-28 19:17:34 +0500

    complete update listing functionality

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   createListing,
   deleteListing,
+  getListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
@@ -9,5 +11,6 @@ const router = Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
-
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 export default router;
